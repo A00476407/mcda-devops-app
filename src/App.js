a58e871c86033b8ce2f2ac2af8457a312b88e52d
Ka Man Leung <kaman.leung@smu.ca>
2023-11-22 10:35:41 -0400

    Update QA

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is PROD.
+          This is QA.
         </p>
       </header>
     </div>
